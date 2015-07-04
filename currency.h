8a6b73daa2286941ded037f8917b29213e4a8ebb
Mikaelie <MIK2130986@maricopa.edu>
2015-07-03 23:36:05 -0700

    Add currency.h

---
+++ currency.h
@@ -0,0 +1,36 @@
+#include<iostream>
+
+class Currency
+{
+  public:
+    int * _dollar;
+    int * _cents;
+  Currency()
+  {
+    _dollar = new int;
+    _cents = new int;
+    *_dollar = 0;
+    *_cents = 0;
+  }
+  Currency(double dollar, double cents)
+  {
+    _dollar = new int;
+    _cents = new int;
+    *_dollar = dollar;
+    *_cents = cents;
+  }
+  Currency(const Currency &original)
+  {
+    _dollar = new int;
+    _cents = new int;
+    *_dollar = *(original._dollar);
+    *_cents = *(original._cents);
+  }
+ ~Currency()
+  {
+    delete(_dollar);
+    delete(_cents);
+    _dollar = NULL;
+    _cents = NULL;
+  }
+};
