c7176029615111365cdd7be0d18c51d1ebdfcb7f
wadehuber <wadehuber@gmail.com>
2015-07-07 23:34:11 -0700

    Main program

---
+++ nmbr220.c
@@ -0,0 +1,17 @@
+// Project 2 Main Program
+
+#include<iostream>
+#include"orderedpair.h"
+using namespace std;
+
+int main () {
+  OrderedPair<T> op(3,4);
+
+  cout << "Hello, World!" << endl;
+
+  cout << "M: ";
+  m.printOrderedPair();
+
+  return 0;
+
+}
