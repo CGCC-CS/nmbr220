fcc666c0de94d06b3de105d932daad31ee3de7af
kegtroll <caesartopher@gmail.com>
2015-07-08 17:38:12 -0700

    updated .h

--- polynomial.h
+++ polynomial.h
@@ -7,16 +7,17 @@ Project 2 - Polynomials - polynomial.h header file
 
 #ifndef __POLYNOMIAL_H__
 #define __POLYNOMIAL_H__
-
+#include<iostream>
 class Polynomial{
 	private:
-		double *;
-		int;
+		double *poly;
+		int order;
 
 	public:
 		Polynomial(int, double *);
 		Polynomial(const Polynomial&);
 		~Polynomial();
+
 		int getOrder();
 		void print();
 
