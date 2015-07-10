c7871b7cae1942874005412d86aa30d1671a4c1e
icleary <[clearyia@gmail.com]>
2015-07-10 12:58:38 -0700

    added Decibel and commented out demo call

--- nmbr220.cpp
+++ nmbr220.cpp
@@ -3,21 +3,25 @@
 #include<iostream>
 #include "numberbase.h"
 #include"orderedpair.h"
+#include"decibel.h"
 using namespace std;
 
-#define COUNT 2
+#define COUNT 3
 
 int main () {
   OrderedPair<int> op1(3,4);
   OrderedPair<float> op2(2.0,4.5);
+  Decibel d1;
   NumberBase *nums[COUNT];
 
   nums[0] = & op1;
   nums[1] = & op2;
+  nums[2] = & d1;
  
-  for (int ii=0;ii<COUNT;ii++)
+  for (int ii=0;ii<COUNT;ii++) {
     nums[ii]->print();
-
+    //nums[ii]->demo();
+  }
   return 0;
 
 }
