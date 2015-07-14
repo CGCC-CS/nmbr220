25bc1c416bd8cf0e1bbe8250d257b45e8b5be742
Mikaelie <MIK2130986@maricopa.edu>
2015-07-13 17:47:32 -0700

    Update nmbr220

--- nmbr220.cpp
+++ nmbr220.cpp
@@ -4,19 +4,22 @@
 #include "numberbase.h"
 #include"orderedpair.h"
 #include"decibel.h"
+#include"currency.h"
 using namespace std;
 
-#define COUNT 3
+#define COUNT 4
 
 int main () {
   OrderedPair<int> op1(3,4);
   OrderedPair<float> op2(2.0,4.5);
   Decibel d1;
+  Currency m1(34,39);
   NumberBase *nums[COUNT];
 
   nums[0] = & op1;
   nums[1] = & op2;
   nums[2] = & d1;
+  nums[3] = & m1;
 
   for (int ii=0;ii<COUNT;ii++) {
     nums[ii]->print();
