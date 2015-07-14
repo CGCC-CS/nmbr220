7b9210c047386716c7f9a469376f508fb7e82302
Wade Huber <wadehuber@gmail.com>
2015-07-13 20:29:26 -0700

    Added grademe()

--- numberbase.h
+++ numberbase.h
@@ -1,8 +1,23 @@
+#include<iostream>
+#include<string>
+
 #ifndef _NUMBERBASE_
 #define _NUMBERBASE_
 class NumberBase {
+   protected: 
+       std::string myGradeRequest;
    public:
+       NumberBase() {
+           myGradeRequest = "I fail";
+       }
+       NumberBase(std::string s) {
+           myGradeRequest = s;
+       }
+       virtual ~NumberBase(void) {};
        virtual void print(void) = 0;
        virtual void demo(void) = 0;
+       virtual void grademe(void) {
+          std::cout << "Grade is " << myGradeRequest << std::endl;
+       }
 };
 #endif
