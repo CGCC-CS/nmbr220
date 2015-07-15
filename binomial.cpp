e50cce58d4b0a627470cddc3c6212377f2638d1e
wowburst <jpaul8@asu.edu>
2015-07-14 21:19:48 -0700

    Update binomial.cpp

--- binomial.cpp
+++ binomial.cpp
@@ -1,5 +1,7 @@
 #include"binomial.h"
 
+#include<iostream>
+
 Binomial::Binomial(int c, int x , int x_2)
 {
 	constant = c;
@@ -8,11 +10,11 @@ Binomial::Binomial(int c, int x , int x_2)
 };
 		
 Binomial::Binomial(const Binomial& copyBinomial)
-		{
-			constant = copyBinomial.getConstant();
-			firstPower = copyBinomial.getFirstPower();
-			secondPower = copyBinomial.getSecondPower();
-		};
+{
+	constant = copyBinomial.getConstant();
+	firstPower = copyBinomial.getFirstPower();
+	secondPower = copyBinomial.getSecondPower();
+};
 		
 int Binomial::getConstant() const
 {
@@ -29,22 +31,36 @@ int Binomial::getSecondPower() const
 	return Binomial::secondPower;
 }
 
-/* Not implemented yet.
-Binomial Binomial::operator+(const Binomial& a, const Binomial& b) {
-	int constant = a.getConstant() + b.getConstant();
-	int firstPower = a.getConstant() + b.getConstant();
-	int secondPower = a.getConstant() + b.getConstant();
-	return Binomial(constant, firstPower, secondPower);
-}
-
-Binomial Binomial::operator*(int a, Binomial b)
+Binomial Binomial::operator+(const Binomial& b)
 {
-	int constant = a * b.getConstant();
-	int firstPower = a * b.getConstant();
-	int secondPower = a * b.getConstant();
+	int constant = this->getConstant() + b.getConstant();
+	int firstPower = this->getFirstPower() + b.getFirstPower();
+	int secondPower = this->getSecondPower() + b.getSecondPower();
 	return Binomial(constant, firstPower, secondPower);
 }
 
-void Binomial::print() {}
-void Binomial::demo() {}
-*/
+Binomial Binomial::operator*(int x)
+{
+	int constant = this->getConstant() * x;
+	int firstPower = this->getFirstPower() * x;
+	int secondPower = this->getSecondPower() * x;
+	return Binomial(constant, firstPower, secondPower);
+}
+
+void Binomial::print()
+{
+	std::cout << this->getConstant() << " + " << this->getFirstPower() << "x + " << this->getSecondPower() << "(x^2)" << std::endl;
+}
+
+void Binomial::demo()
+{
+	int three = 3;
+	Binomial bin(6, 7, 8);
+	Binomial bin2(bin);
+	Binomial bin3 = bin + bin2;
+	Binomial bin4 = bin * 3;
+	bin.print();
+	bin2.print();
+	bin3.print();
+	bin4.print();
+}
