909792453f6de4bf7f10f7170dd18f833c102ef4
kegtroll <caesartopher@gmail.com>
2015-07-14 22:22:09 -0700

    Updated C++ file and header file

--- polynomial.cpp
+++ polynomial.cpp
@@ -7,31 +7,22 @@ Project 2 - polynomial.cpp
 
 #include<iostream>
 #include"numberbase.h"
-
+#include"polynomial.h"
 using namespace std;
 
-class Polynomial : public NumberBase{
-
-  private:
-	double *poly;
-	int order;
-
-  public:
-	// constructor
-        Polynomial(int size = 0, double * plyIn = 0) :						 NumberBase("Chris Ward - A") {
-	  if(size > 0){
+//Constructor
+Polynomial::Polynomial(int size = 0, double * plyIn = 0) :				 NumberBase("Chris Ward - A") {
+	if(size > 0){
 	  poly = new double[(size + 1)];
 	  for(int ii = 0; ii < (size + 1); ii++){
 	  	poly[ii] = plyIn[ii];
-	        //cout << poly[ii] << " , " << plyIn[ii] << endl;
 	  }
 	  order = size ;
-	  	//cout << "sizeof order: " << order << endl;
 	  }
 	}
 	
-	// copy constructor
-	Polynomial(const Polynomial& og){
+// Copy Constructor
+Polynomial::Polynomial(const Polynomial& og){
 	  poly = new double[(og.order + 1)];
 	  for(int ii = 0; ii < og.order; ii++){
 	    poly[ii] = (og.poly[ii]);
@@ -39,58 +30,27 @@ class Polynomial : public NumberBase{
 	  order = og.order;
 	}
 
-	// destructor
-	~Polynomial(){
+// Destructor
+Polynomial::~Polynomial(){
 	  delete(poly);
 	}
 
-	// getter for order
-	int getOrder();
-	void print();
-	void demo();
-	
-	void polyAdd(int);
-	// Override =
-	Polynomial& operator=(const Polynomial&);
-
-	// Override +
-	Polynomial operator+(int);
-        friend Polynomial operator+(const Polynomial&);
-        friend Polynomial operator+(int,const Polynomial&);
-        friend Polynomial operator+(double,const Polynomial&);
-
-        // Override -
-        Polynomial operator-(int);
-        Polynomial operator-(double);
-        Polynomial operator-(const Polynomial&);
-
-        // Override *
-        Polynomial operator*(int);
-        Polynomial operator*(double);
-        Polynomial operator*(const Polynomial&);
-
-        // Override /
-        Polynomial operator/(int);
-        Polynomial operator/(double);
-        Polynomial operator/(const Polynomial&);
-
-
-};
-
+// Getter for order
 int Polynomial::getOrder(){
 	return(order);
 }
+
+// Add function, was used for initially testing add logic.
 void Polynomial::polyAdd(int num){
 	if(order > 0){	
 	   poly[order] = poly[order] + num;
 	}
 	else{
 	   poly[0] = num;}
-
 }
 
+// Override =
 Polynomial& Polynomial::operator=(const Polynomial &that){
-	cout << that.poly[that.order] << endl;
         for(int ii = 0; ii <= that.order; ii++){
             poly[ii] = (that.poly[ii]);
           }
@@ -98,6 +58,22 @@ Polynomial& Polynomial::operator=(const Polynomial &that){
 	return *this;
 }
 
+// Override <<
+ostream& operator<<(ostream &stream,const Polynomial& that){
+	if(that.order > 0){
+          int cc = 0;
+          for(int ii = that.order; ii >= 0 ; ii--){
+                stream << that.poly[cc] << "x^" << ii << " " ;
+                cc++;
+          }
+        }
+        else{
+          stream << "0x^x";
+        }
+	return stream;
+}	
+
+// Override + for int
 Polynomial Polynomial::operator+(int num){
 	Polynomial ret(this->getOrder(), this->poly);
 	if(ret.order > 0){
@@ -107,13 +83,167 @@ Polynomial Polynomial::operator+(int num){
 	  ret.poly[0] = num;
 	}
 	return ret;
+}
+
+// Override + for double
+Polynomial Polynomial::operator+(double num){
+	Polynomial ret(this->getOrder(), this->poly);
+	if(ret.order > 0){
+	  ret.poly[ret.getOrder()] += num;
+	}
+	else{ 
+	  ret.poly[0] = num;
+	}
+	return ret;
+}
+
+// Override + for another Polynomial
+Polynomial Polynomial::operator+(const Polynomial& that){
+	Polynomial ret(this->getOrder(), this->poly);
+	if(ret.order > that.order){
+	  for(int ii = 0; ii <= that.order; ii++)
+		ret.poly[ii+that.order] += that.poly[ii];
+	}
+	else if(ret.order < that.order){
+	  for(int ii = 0; ii <= ret.order; ii++)
+		ret.poly[ii] += that.poly[ii+ret.order];
+	}
+	else if(ret.order == that.order){
+	  for(int ii = 0; ii <= ret.order; ii++)
+		ret.poly[ii] += that.poly[ii];
+	}
+	return ret;
+}
+
+// Override + for int previous to a polynomial
+Polynomial operator+(int num,const Polynomial& that){
+	Polynomial ret(that.order, that.poly);
+	if(ret.order > 0){
+	  ret.poly[ret.getOrder()] += num;
+	}
+	else{ 
+	  ret.poly[0] = num;
+	}
+	return ret;
+}
+
+// Override + for double previous to a polynomial
+Polynomial operator+(double num,const Polynomial& that){
+	Polynomial ret(that.order, that.poly);
+	if(ret.order > 0){
+	  ret.poly[ret.getOrder()] += num;
+	}
+	else{ 
+	  ret.poly[0] = num;
+	}
+	return ret;
+}
+
+// Override - for an int
+Polynomial Polynomial::operator-(int num){
+	Polynomial ret(this->getOrder(), this->poly);
+	if(ret.order > 0){
+	  ret.poly[ret.getOrder()] -= num;
+	}
+	else{ 
+	  ret.poly[0] = num;
+	}
+	return ret;
+}
+
+// Override - for a double
+Polynomial Polynomial::operator-(double num){
+	Polynomial ret(this->getOrder(), this->poly);
+	if(ret.order > 0){
+	  ret.poly[ret.getOrder()] -= num;
+	}
+	else{ 
+	  ret.poly[0] = num;
+	}
+	return ret;
+}
+
+// Override - for another polynomial
+Polynomial Polynomial::operator-(const Polynomial& that){
+	Polynomial ret(this->getOrder(), this->poly);
+	if(ret.order > that.order){
+	  for(int ii = 0; ii <= that.order; ii++)
+		ret.poly[ii+that.order] -= that.poly[ii];
+	}
+	else if(ret.order < that.order){
+	  for(int ii = 0; ii <= ret.order; ii++)
+		ret.poly[ii] -= that.poly[ii+ret.order];
+	}
+	else if(ret.order == that.order){
+	  for(int ii = 0; ii <= ret.order; ii++)
+		ret.poly[ii] -= that.poly[ii];
+	}
+	return ret;
+}
+
+// Override - for int previous to a polynomial
+Polynomial operator-(int num,const Polynomial& that){
+	Polynomial ret(that.order, that.poly);
+	if(ret.order > 0){
+	  ret.poly[ret.getOrder()] -= num;
+	}
+	else{ 
+	  ret.poly[0] = 0 - num;
+	}
+	return ret;
+}
+
+// Override - for a double previous to a polynomial
+Polynomial operator-(double num,const Polynomial& that){
+	Polynomial ret(that.order, that.poly);
+	if(ret.order > 0){
+	  ret.poly[ret.getOrder()] -= num;
+	}
+	else{ 
+	  ret.poly[0] = 0 - num;
+	}
+	return ret;
+}
+
+// Override * for an int, scalar multiplication
+Polynomial Polynomial::operator*(int num){
+	Polynomial ret(this->getOrder(), this->poly);
+	for(int ii = 0; ii <= ret.order; ii++)
+	  ret.poly[ii] *= num;
+	return ret;
+}
+
+// Override * for a double, scalar multiplication
+Polynomial Polynomial::operator*(double num){
+	Polynomial ret(this->getOrder(), this->poly);
+	for(int ii = 0; ii <= ret.order; ii++)
+	  ret.poly[ii] *= num;
+	return ret;
 
 }
-Polynomial operator+(int,const Polynomial &p){cout << "int, poly" << endl;}
-Polynomial operator+(double,const Polynomial &p){}
-Polynomial operator+(const Polynomial &p, double){}
 
-//make this as an ostream operator override possible
+// Override * for a polynomial. Polynomial Multiplication
+Polynomial Polynomial::operator*(const Polynomial& that){
+
+}
+
+// Override * for an int previous to a polynomial
+Polynomial operator*(int num,const Polynomial& that){
+	Polynomial ret(that.order,that.poly);
+	for(int ii = 0; ii <= ret.order; ii++)
+	  ret.poly[ii] *= num;
+	return ret;
+}
+
+// Override * for a double previous to a polynomial
+Polynomial operator*(double num,const Polynomial& that){
+	Polynomial ret(that.order, that.poly);
+	for(int ii = 0; ii <= ret.order; ii++)
+	  ret.poly[ii] *= num;
+	return ret;
+}
+
+// Print function
 void Polynomial::print(){
 	if(this->order > 0){
 	  int cc = 0;
@@ -128,6 +258,7 @@ void Polynomial::print(){
 	cout << endl;
 }
 
+// Demo the polynomial class
 void Polynomial::demo(){
 
 }
@@ -137,14 +268,11 @@ void Polynomial::demo(){
 //main for debugging purposes
 int main(){
 	double APoly1[5] ={1.0,1.5,6.2,10.2,12.2};
-	//Polynomial  poly1 = new Polynomial(4,APoly1);
-	//cout << "p1 done\n";
-	//Polynomial  poly2 = new Polynomial();
-	//Polynomial  poly3(poly1);
-	//cout << "p2 done\n";
+	double APoly2[3] = {4.2,14.4,54.0};
 	Polynomial poly1(4, APoly1);
 	Polynomial poly2(4, APoly1);
 	Polynomial poly3(4, APoly1);
+	Polynomial poly4(2, APoly2);
 
 	//poly1.print();
 	//poly2.print();
@@ -153,12 +281,62 @@ int main(){
 	cout << "p1 before addition of 10" << endl;
 	poly1.print();
 	cout << "adding 10 to p1, storing as p2" << endl;
+	
+	//Testing polynomial + int
 	poly2 = poly1 + 10;
 	cout << "P1 after add:" << endl;
 	poly1.print();
 	cout << "P2 after add:" << endl;
 	poly2.print();
 
+	//Testing int + polynomial
+	cout << endl << "10 + P2:\n";
+	poly2 = 10 + poly2;
+	poly2.print();	
+
+	//Testing polynomial + polynomial same order
+	cout << endl << "P1 + P2:\n";
+	poly3 = poly1 + poly2;
+	poly3.print();
+
+	//Testing polynomial + polynomial w/ different sized polynomials
+	cout << endl << "P1 & P4\n";
+	poly1.print();
+	poly4.print();
+	cout << "P1 + P4\n";
+	poly3 = poly1 + poly4;
+	poly3.print();
+
+	//Testing polynomial - int
+	cout << endl << "P1 - 10:\n";
+	poly3 = poly1 - 10;
+	poly3.print();
+
+	//Testing int - polynomial
+	cout << endl << "10 - P1\n";
+	poly3 = 10 - poly1;
+	poly3.print();
+
+	//Testing polynomial - polynomial different orders	
+	cout << endl << "P1 & P4\n";
+	poly1.print();
+	poly4.print();
+	cout << "P1 - P4\n";
+	poly3 = poly1 - poly4;
+	poly3.print();
+
+	//Testing polynomial * int
+	cout << endl << "P1 * 10\n";
+	poly3 = poly1 * 10;
+	poly3.print();
+
+	//Testing int * polynomial
+	cout << endl << "10 * P1\n";
+	poly3 = 10 * poly1;
+	poly3.print();
+
+	//Testing <<
+	cout << endl << poly1 << endl;
 	return 0;
 }
 
