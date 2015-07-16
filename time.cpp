a8229f7fef33a055bbc254e5b3fe20a7d71ccca2
James McAdams <JAMUW93901@maricopa.edu>
2015-07-16 09:35:16 -0700

    Working

--- time.cpp
+++ time.cpp
@@ -1,104 +1,257 @@
-/*  James McAdams
-*   Project 2
-*   CSC220 - Summer 2015
-*   07/10/2015
-*/
+/* James McAdams:
+*  Project #2 - Overridden Operators
+*  Mathmatical Functions of Time
+*  CSC220 - Summer 2015
+*  07/14/2015
+*
+*  time.cpp
+*
+*/ 
 
 #include <iostream>
+#include <string.h>
+#include <cmath>
+#include "time.h"
+#include "numberbase.h"
 
 using namespace std;
 
-class Time
+// ----------------------- Constructors - Begin -------------------------  
+
+//Default Constructor
+
+Time::Time () : NumberBase ( ) {
+    this->d1 = 1;
+    this->h1 = 1;
+    this->m1 = 1;
+    this->ap1 = 1;
+}
+Time::Time (int d, int h, int m, int ap) : NumberBase ( )
+  {
+    int ht1;
+    d1 = d;
+    h1 = h;
+    m1 = m;
+    ap1 = ap;
+    cout << "Default Constructor" << endl;
+    cout << "Time Input #1: " << d1 << " Days, " << h1 << " Hours, " << m1 << " Minutes, and " << ap1 << endl;                  
+  }
+
+//Copy Constructor
+Time::Time (const Time& base)
+  {
+    d1 = base.get_d();
+    h1 = base.get_h();
+    m1 = base.get_m();
+    ap1 = base.get_ap();
+  
+    cout << "Copy Constructor" << endl;
+    cout << "Time Input: " << d1 << " Days, " << h1 << " Hours, " << m1 << " Minutes, and " << ap1 << endl;                  
+  }
+
+//Sum Constructor
+Time::Time (int d1, int d2, int h1, int h2, int m1, int m2, int ap1, int ap2, int larger) : NumberBase ( )
+  {
+    
+    d_d = d1 + d2;
+    h_d = h1 + h2;
+    m_d = m1 + m2;
+    ap_d = ap1 + ap2;
+    if ((ap1 == 1) && (ap2 == 0))
+      {h_d = h_d - 12;}
+    if ((ap2 == 1) && (ap1 == 0))
+      {h_d = h_d + 12;}
+    cout << "Sum Constructor" << endl;
+    cout << "Verification: d_d = " << d_d << " ; h_d = " << h_d << " ; m_d = " << m_d << " ; ap_d = " << ap1 << endl; 
+  }
+
+//Difference Constructor
+Time::Time (int d1, int d2, int h1, int h2, int m1, int m2, int ap1, int ap2) : NumberBase ( )
+  {
+    
+    d_d = d1 - d2;
+    h_d = h1 - h2;
+    m_d = m1 - m2;
+    ap_d = ap1 - ap2;
+    if ((ap1 == 1) && (ap2 == 0))
+      {h_d = h_d + 12;}
+    if ((ap2 == 1) && (ap1 == 0))
+      {h_d = h_d - 12;}
+    cout << "Difference Constructor" << endl;
+    cout << "Verification: d_d = " << d_d << " ; h_d = " << h_d << " ; m_d = " << m_d << " ; ap_d = " << ap1 << endl; 
+  }
+
+//Multiple Constructor
+Time::Time (int d, int h, int m, int ap, float mult) : NumberBase ( )
+  {
+    d1 = 27;
+    h1 = 5;
+    m1 = 27;
+    ap1 = 1;
+ 
+    cout << "Default Constructor" << endl;
+    cout << "Verification: d1 = " << d1 << " ; h1 = " << h1 << " ; m1 = " << m1 << " ; ap1 = " << ap1 << endl; 
+  }
+
+//Destructor
+Time::~Time(){}
+
+
+// ----------------------- Getters/Setters - Begin ---------------  
+
+int Time::get_d(void) const
+  {
+    return d1;
+  }
+int Time::get_h(void) const
+  {
+    return h1;
+  }
+int Time::get_m(void) const
+  {
+    return m1;
+  }
+int Time::get_ap(void) const
+  {
+    return ap1;
+  }
+void Time::set_d(int di)
+  {
+    d = di;
+  }
+void Time::set_h(int hi)
+  {
+    h = hi;
+  }
+void Time::set_m(int mi)
+  {
+    m = mi;
+  }
+void Time::set_ap(int api)
+  {
+    ap = api;
+  }
+// ----------------------- Getters/Setters - End  ----------------  
+
+
+
+// ---------------- Operator Overloading - Begin  ----------------  
+
+Time Time::operator=( const Time &that)
+  {
+    d1 = that.get_d();
+    h1 = that.get_h();
+    m1 = that.get_m();
+    ap1 = that.get_ap();
+    return *this;
+  }
+Time Time::operator+( const Time &that)
+  {
+    Time k;
+    k.set_d(get_d() + that.get_d());
+    k.set_d(get_h() + that.get_h());
+    k.set_d(get_m() + that.get_m());
+    k.set_d(get_ap() + that.get_ap());
+    return k;
+  }
+Time Time::operator-( const Time &that)
+  {
+    Time k;
+    k.set_d(get_d() - that.get_d());
+    k.set_d(get_h() - that.get_h());
+    k.set_d(get_m() - that.get_m());
+    k.set_d(get_ap() - that.get_ap());
+    return k;
+  }
+
+Time Time::operator*( const Time &that)
+  {
+    Time k;
+    k.set_d(get_d() * that.get_d());
+    k.set_d(get_h() * that.get_h());
+    k.set_d(get_m() * that.get_m());
+    k.set_d(get_ap() * that.get_ap());
+    return k;
+  }
+
+
+// ---------------- Operator Overloading - End  ------------------  
+
+
+
+// --------------------- Print - Begin ---------------------------
+void Time::print()
+  {
+      cout << "Time Input #1: " << this->d1 << " Days, " << this->h1 << ":" << this->m1<< " " <<this->ap1 << endl;
+ 
+  return;
+  }
+
+  
+// --------------------- Print - End -----------------------------  
+
+
+// ----------------------- Demo - Begin --------------------------  
+
+
+
+void Time::demo (void)
+  {
+  d = 4;
+  h = 9;
+  m = 15;
+  ap = 1;
+  
+  Time t1(d,h,m,ap);
+  cout << "t1 is initializing." << endl;
+  t1.print();
+  
+  d = 4;
+  h = 9;
+  m = 15;
+  ap = 1;
+  Time t2(d,h,m,ap);
+  cout << "t2 is initializing." << endl;
+  t2.print();
+  
+  Time t3 = t2;
+  cout << "t3 is a copy of t2, accomplished by overriding the = operator." << endl;
+  t3.print();
+  
+  Time t4;
+  t4 = t1 - t2;
+  cout << "t4 is subtracting the larger of the 2 times from the other to determine the difference." << endl;
+  cout << "This is accomplished with the overridden - operator." << endl;
+  t4.print();
+  
+  Time t5;
+  t5 = t1 + t2; 
+  cout << "t5 is adding the raw values of t1 and t2 together." << endl;
+  cout << "This is accomplished with the overridden + operator." << endl;
+  t5.print();
+  
+  Time t6;
+  t6 = t1 * t2;
+  cout << "t6 is multiplying the raw values of t1 and t2 together." << endl;
+  cout << "This is accomplished with the overridden * operator." << endl;
+  t6.print();
+
+  return;
+
+  }
+
+// ----------------------- Demo - End ----------------------------  
+
+
+// ----------------------- Main - Begin --------------------------  
+
+int main(void)
 {
-   public:
-      int h,m,ap;
-      int getTime(int h, int m, int ap);
-      int display();
-      int geth();
-      int getm();
-      int getap();
-      Time( int h, int m, int ap);                                // primary constructor
-      Time( const Time t1, const Time t2, const Time t_am);     // copy constructor
-      ~Time();                                    // destructor
+  Time t1(1,1,1,1);
+  t1.demo();	
 
-   private:
-      int h_t;
-      int m_t;
-      int ap_t;
-}
-;
-
-int Time::geth() {
-  return h;
-}
-int Time::getm() {
-  return m;
-}
-int Time::getap() {
-  return ap;
-}
-
-// Member functions definitions including constructor
-
-Time::Time(int h, int m, int ap)
-{
-        h = 12;
-        m = 45;
-        ap = 1;    //AM = 0; PM = 1;
-//        h_t = h;
-//        m_t = m;
-//        ap_t = ap;
-//        cout << h_t << endl;
-//        cout << m_t << endl;
-//        cout << ap_t << endl;
-}
-
-// Copy Constructor ----- Start -------------------------
-Time::Time(Time t1, Time t2, Time t3)
-{
-        //h_t = t1;
-        //m_t = t2;
-        //ap_t = t3;
-        cout << h_t << endl;
-        cout << m_t << endl;
-        cout << ap_t << endl;
-
-}
-// Copy Constructor ----- End --------------------------
-
-Time::~Time()
-{
-   cout << "Deconstructor" << endl;
-   //delete h_t;
-   //delete m_t;
-   //delete ap_t;
-}
-
-int Time::getTime(int h,int m, int ap)
-{
-  cout << "Time::getTime" << endl;
-  cout << "Hour: " << h_t << endl;
-  return h_t;
-  return m_t;
-  return ap_t;
-}
-
-int Time::display()
-{
-    cout << "Time::display" << endl;
-//  cout << "Hour: " << h_t << endl;
-//  cout << "Minutes: " << m_t << endl;
-//  cout << "AM/PM: " << ap_t << endl;
-}
-
-// Main function for the program
-int main( )
-{
-
-  Time * time = (12,30,0);
-  Time getTime (12,20,0);
-  //Time display();
-  cout << "I actually ran" << endl;
   return 0;
-
 }
+
+
+// ----------------------- Main - End ----------------------------  
--- time.h
+++ time.h
@@ -1,55 +1,69 @@
-/*  James McAdams
-*   Project 2
-*   CSC220 - Summer 2015
-*   07/10/2015
-*/
-#include<iostream>
+/* James McAdams
+*  Project #2 - Overridden Operators
+*  Mathmatical Functions of Time
+*  CSC220 - Summer 2015
+*  07/14/2015
+*
+*  time.h
+*
+*/ 
+
+#ifndef _TIME_
+#define _TIME_
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "numberbase.h"
 
 using namespace std;
 
+// ----------------------- Class - Begin -------------------------  
 
-// I'm working on it. Bare with me please...
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-~                                                                                                                                                                                       
-"time.h" 11L, 163C                                                                                                                                                    1,1           All
+class Time : public NumberBase 
+
+{
+private:
+        int d, h, m, ip, ap, ht1, ht2;
+	int d1, h1, m1, ap1;
+	int d2, h2, m2, ap2;
+        int di, hi, mi, api, ipi;
+	int d_o, h_o, m_o, ap_o;
+        int d_d, h_d, m_d, ap_d;
+        char ap_c[3];
+        float mult;	
+public:
+	Time ();
+        Time (int d, int h, int m, int ap);
+        Time (int d, int h, int m, int ap, int ip);
+        Time (int d, int h, int m, int ap, float mult);
+        Time (int d1, int d2, int h1, int h2, int m1, int m2, int ap1, int ap2);
+        Time (int d1, int d2, int h1, int h2, int m1, int m2, int ap1, int ap2, int larger);
+        Time (const Time& base)/* : NumberBase ( )*/;
+        ~Time();
+
+// ----------------------- Getters/Setters - Begin ---------------  
+
+int get_d() const;
+int get_h() const;
+int get_m() const;
+int get_ap() const;
+void set_d(int di);
+void set_h(int hi);
+void set_m(int mi);
+void set_ap(int api);
+// ----------------------- Getters/Setters - End  ----------------  
+
+// -------------- Operator Overloading - Begin --------------------
+    Time operator=(const Time &that);
+    Time operator+(const Time &that);
+    Time operator-(const Time &that);
+    Time operator*(const Time &that);
+    friend ostream& operator<<(ostream &strm, const Time &that);
+// -------------- Operator Overloading - End ----------------------
+
+void print();
+void demo();
+
+};
+#endif
